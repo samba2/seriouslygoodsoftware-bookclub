04ba58bec1b11ccd3fc748d04fe558d3ec3c4aec
Maik Toepfer <maik.toepfer@prisma-capacity.eu>
2020-12-30 00:34:16 +0100

    working implementation

--- src/main/java/org/samba/seriouslygoodsw/WaterContainer.java
+++ src/main/java/org/samba/seriouslygoodsw/WaterContainer.java
@@ -1,42 +1,81 @@
 package org.samba.seriouslygoodsw;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WaterContainer {
 
-    private List<WaterContainer> connectedContainers = new ArrayList<>();
+    private Set<WaterContainer> neighbors = new HashSet<>();
     private long amount;
+    private String name;
 
-    public WaterContainer() {
-        connectedContainers.add(this);
+
+    public WaterContainer(String name) {
+        this.name = name;
     }
 
     public void addWater(long newWater) {
         this.amount += newWater;
+        // TODO distribute also here + test
     }
 
     public void connectTo(WaterContainer neighbour) {
-        this.connectedContainers.add(neighbour);
+        this.newNeighbor(neighbour);
+        neighbour.newNeighbor(this);
+
         distributeWater();
     }
 
+    void newNeighbor(WaterContainer neighbour) {
+        this.neighbors.add(neighbour);
+    }
+
     private void distributeWater() {
-        var totalSum = connectedContainers.stream()
+        var allConnectedContainers = getNeighbors(this, this);
+        allConnectedContainers.add(this);
+
+        var totalSum = allConnectedContainers.stream()
                 .map(waterContainer -> waterContainer.getAmount())
                 .mapToLong(Long::longValue)
                 .sum();
 
-        long newAmount = totalSum / connectedContainers.size();
+        long newAmount = totalSum / allConnectedContainers.size();
 
-        connectedContainers.forEach(waterContainer -> waterContainer.setAmount(newAmount));
+        allConnectedContainers.forEach(waterContainer -> waterContainer.setAmount(newAmount));
     }
 
-    public void setAmount(long amount) {
+    Set<WaterContainer> getNeighbors(WaterContainer originalCaller, WaterContainer caller) {
+        if (! caller.equals(originalCaller) && this.equals(originalCaller)) return Collections.emptySet();
+
+        var foundNeighbors = new HashSet<WaterContainer>();
+        // add neighbors except caller and original caller
+        foundNeighbors.addAll(neighbors);
+        foundNeighbors.remove(caller);
+        foundNeighbors.remove(originalCaller);
+
+        // continue finding neighbors neighbors excluding the caller (infinite loop)
+        for (WaterContainer c : neighbors) {
+            if (c.equals(caller)) continue;
+            foundNeighbors.addAll(c.getNeighbors(originalCaller, this));
+        }
+
+        return foundNeighbors;
+    }
+
+    protected void setAmount(long amount) {
         this.amount = amount;
     }
 
     public long getAmount() {
         return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "WaterContainer{" +
+                "amount=" + amount +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/test/java/org/samba/seriouslygoodsw/WaterContainerTest.java
+++ src/test/java/org/samba/seriouslygoodsw/WaterContainerTest.java
@@ -1,24 +1,82 @@
 package org.samba.seriouslygoodsw;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WaterContainerTest {
 
     @Test
     public void emptyContainer() {
-        var a = new WaterContainer();
+        var a = new WaterContainer("A");
         assertEquals(0, a.getAmount());
     }
 
 
     @Test
+    public void singleContainerHasNoNeighbors() {
+        var a = new WaterContainer("A");
+        assertEquals(Collections.emptySet(), a.getNeighbors(a, a));
+    }
+
+    @Test
+    public void directNeighbor() {
+        var a = new WaterContainer("A");
+        var b = new WaterContainer("B");
+        a.connectTo(b);
+        assertEquals(Set.of(b), a.getNeighbors(a, a));
+        assertEquals(Set.of(a), b.getNeighbors(b, b));
+    }
+
+    @Test
+    public void twoNeighbors() {
+        var a = new WaterContainer("A");
+        var b = new WaterContainer("B");
+        var c = new WaterContainer("C");
+        b.connectTo(a);
+        b.connectTo(c);
+
+        assertEquals(Set.of(a, c), b.getNeighbors(b, b));
+        assertEquals(Set.of(b, c), a.getNeighbors(a, a));
+        assertEquals(Set.of(a, b), c.getNeighbors(c, c));
+    }
+
+    @Test
+    public void indirectNeighbor() {
+        var a = new WaterContainer("A");
+        var b = new WaterContainer("B");
+        var c = new WaterContainer("C");
+        a.connectTo(b);
+        b.connectTo(c);
+
+        assertEquals(Set.of(a, c), b.getNeighbors(b, b));
+        assertEquals(Set.of(b, c), a.getNeighbors(a, a));
+        assertEquals(Set.of(a, b), c.getNeighbors(c, c));
+    }
+
+    @Test
+    public void cutLoop() {
+        var a = new WaterContainer("A");
+        var b = new WaterContainer("B");
+        var c = new WaterContainer("C");
+        a.connectTo(b);
+        b.connectTo(c);
+        c.connectTo(a);
+
+        assertEquals(Set.of(a, c), b.getNeighbors(b, b));
+        assertEquals(Set.of(b, c), a.getNeighbors(a, a));
+        assertEquals(Set.of(a, b), c.getNeighbors(c, c));
+    }
+
+    @Test
     public void initialFill() {
-        var a = new WaterContainer();
-        var b = new WaterContainer();
-        var c = new WaterContainer();
-        var d = new WaterContainer();
+        var a = new WaterContainer("A");
+        var b = new WaterContainer("B");
+        var c = new WaterContainer("C");
+        var d = new WaterContainer("D");
 
         a.addWater(12);
         d.addWater(8);
@@ -30,5 +88,42 @@ public class WaterContainerTest {
         assertEquals(8, d.getAmount());
     }
 
+
+    @Test
+    public void secondStep() {
+        var a = new WaterContainer("A");
+        var b = new WaterContainer("B");
+        var c = new WaterContainer("C");
+        var d = new WaterContainer("D");
+
+        a.addWater(12);
+        d.addWater(8);
+        a.connectTo(b);
+        b.connectTo(c);
+
+        assertEquals(4, a.getAmount());
+        assertEquals(4, b.getAmount());
+        assertEquals(4, c.getAmount());
+        assertEquals(8, d.getAmount());
+    }
+
+    @Test
+    public void thirdStep() {
+        var a = new WaterContainer("A");
+        var b = new WaterContainer("B");
+        var c = new WaterContainer("C");
+        var d = new WaterContainer("D");
+
+        a.addWater(12);
+        d.addWater(8);
+        a.connectTo(b);
+        b.connectTo(c);
+        b.connectTo(d);
+
+        assertEquals(5, a.getAmount());
+        assertEquals(5, b.getAmount());
+        assertEquals(5, c.getAmount());
+        assertEquals(5, d.getAmount());
+    }
 
 }
